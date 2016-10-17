d7f9f3c6036ab0c83bb38c5fc45254c09ebf17f2
Jesse McKay and Taylor Loftis-Kim <jesse@>
2016-10-17 14:25:12 -0700

    add index.html, styles.css and scripts.js, add practice file for javascript

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter a number:"));
+
+alert(add(number1, number2));
+
+Writing the alert into a variable to add
+var result = add(number1, number2);
+alert(result);
+
+
+
+// our body mass function begins here
+      // var bodyMass = function(height, weight) {
+      //   return (weight * 0.45) / ((height * 0.025) * (height * 0.025));
+      // };
+      //
+      // var height = parseFloat(prompt("How tall are you in inches?"));
+      // var weight = parseFloat(prompt("How much do you weigh in lbs?"));
+      //
+      // alert(bodyMass(height,weight));
+      //
+
+//  fahrenheit conversion function begins here
+    // var fahrenheit = parseFloat(prompt("What is your temperature in fahrenheit?"));
+    // var conversion = function(fahrenheit) {
+    // 	return (fahrenheit - 32) * (5/9);
+    // };
+    //
+    // alert(conversion(fahrenheit));
+
+
+
+
+
+
+
+
+
+
+    
